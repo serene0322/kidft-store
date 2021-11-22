f593cc92f2a8bbe969c0637e340318a1589f8819
serene0322 <musachan322@gmail.com>
2021-11-22 12:24:51 +0800

    add the modify quantity feature in checkout component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -7,14 +7,34 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     //if exist, return a new array (new version of state) so can re-render
     if (existingCartItem) {
-        return cartItems.map(cartItem => 
+        return cartItems.map(cartItem =>
             //if same item, create a new object which will have the cart item and quantity + 1
             cartItem.id === cartItemToAdd.id
-            ? { ...cartItem, quantity: cartItem.quantity + 1}
-            //else of not match, just return the original cart item
-            : cartItem
-            )
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                //else of not match, just return the original cart item
+                : cartItem
+        )
     }
     //if the cart item is not found inside array, return a new array with all existing cart items that already there
-    return [...cartItems, { ...cartItemToAdd, quantity: 1}]
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+    
+    //remove the item from cart if it is the last quantity
+    if (existingCartItem.quantity === 1) {
+        //if the id not the one need remove, keep the item
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    //if quantity is not 1 and we know there is existing cart item
+    return cartItems.map(
+        cartItem =>
+            cartItem.id === cartItemToRemove.id ?
+                { ...cartItem, quantity: cartItem.quantity - 1 }
+                : cartItem
+    );
 };

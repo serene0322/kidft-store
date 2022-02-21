84db14cc88a00ee9a884949ef37e2dc331cb19ff
serene0322 <musachan322@gmail.com>
2022-02-21 20:24:33 +0800

    modify package.json and gitgnore file

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Running");
+  res.send("Server Running");
 });
 
 io.on("connection", (socket) => {

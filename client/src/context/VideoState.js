fc0cb04c3e1f9759296ed89a699d0b9131cbbfba
serene0322 <musachan322@gmail.com>
2022-02-26 16:16:56 +0800

    try heroku server

--- client/src/context/VideoState.js
+++ client/src/context/VideoState.js
@@ -6,7 +6,8 @@ import Peer from "simple-peer";
 import { message } from "antd";
 
 //const URL = "http://localhost:5000/";
-const URL = "https://kidft-store.herokuapp.com/";
+//const URL = "https://kidft-store.herokuapp.com/";
+const URL = "https://kidft-store-client-server.herokuapp.com/";
 
 export const socket = io(URL);
 

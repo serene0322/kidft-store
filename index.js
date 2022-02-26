dae54cfb237627ef89f11f35ca802eadc213365a
serene0322 <musachan322@gmail.com>
2022-02-26 15:42:31 +0800

    modify index.js to try heroku

--- index.js
+++ index.js
@@ -28,8 +28,11 @@ app.use(cors()); //cross origin request
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  // app.get('*', function(req, res) {
+  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  // });
+  app.get("/", (req, res) => {
+    res.send("Server Running");
   });
 }
 

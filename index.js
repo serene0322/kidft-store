36d6da695bc5bfe02057b0770742150a5b834891
serene0322 <musachan322@gmail.com>
2022-02-26 16:11:55 +0800

    try heroku

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors()); //cross origin request
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  //app.use(express.static(path.join(__dirname, 'client/build')));
 
   // app.get('*', function(req, res) {
   //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
